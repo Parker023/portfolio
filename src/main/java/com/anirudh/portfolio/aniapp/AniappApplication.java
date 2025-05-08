1c5e7200e1f56c7473dbe3c3e040446e842eb9d2
Anirudh <amirudhshanmukha2399@gmail.com>
2025-05-08 07:55:13 +0530

    initial commit

---
+++ src/main/java/com/anirudh/portfolio/aniapp/AniappApplication.java
@@ -0,0 +1,13 @@
+package com.anirudh.portfolio.aniapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AniappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AniappApplication.class, args);
+	}
+
+}
